d42d448b626126149c20272a3b89d6ce1b8f4e09
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-08-27 15:54:31 -0300

    .
    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
 
 export function App() {
   return (
     <>
-      <h1>Testando</h1>
+      <Header />
       <GlobalStyle />
     </>
   );
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,14 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
-
+    --blue: #5429cc;
+    --red: #e52e4d;
+    --blue-light: #6933ff;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    --background: #f0f2f5;
+    --shape: #FFFFFF;
+    //--green: #
   }
   *{
     margin: 0;
@@ -11,9 +18,35 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  html{
+    @media(max-width:1080px){
+      font-size: 93.75%;
+    }
+    @media(max-width:720px){
+      font-size: 87.5%;
+    }
+  }
+
   body {
     background: var(---background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: "Poppins" , sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+  [disabled]{
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 
 `
