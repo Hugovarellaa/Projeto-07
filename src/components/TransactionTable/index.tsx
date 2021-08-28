9ebfaae561ab46f96d1cec5fef1eb383832df9c8
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-08-28 18:38:59 -0300

    .
    .

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -2,11 +2,21 @@ import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: string;
+  title: string;
+  amount: number;
+  category: string;
+  type: string;
+  createAt: string;
+}
+
 export function TransactionTable() {
-  const [transactions, setTransactions] = useState([]);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
-    api.get("/transactions").then((response) => setTransactions(response.data));
+    api.get("/transactions")
+    .then(response => setTransactions(response.data.transactions));
   }, []);
 
   return (
@@ -23,11 +33,20 @@ export function TransactionTable() {
         <tbody>
           {transactions.map((transaction) => {
             return (
-              <tr>
+              <tr key={transaction.id}>
                 <td>{transaction.title}</td>
-                <td className="deposit">{transaction.amount}</td>
+                <td className={transaction.type}>
+
+                  {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                  }).format(transaction.amount)}
+
+                  </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createAt} </td>
+                <td>
+                  {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createAt))}
+                </td>
               </tr>
             );
           })}
